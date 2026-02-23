4b23729edcd33715c33bc9b163d89c4523032b6b
jmiguelcol27 <jmiguelcol2712@gmail.com>
2026-02-22 21:35:53 -0600

    feat: brand page as Balloonie and keep calculator textarea-only

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SITE_CONFIG = {
+  whatsappPhoneE164: "17865992191",
+  defaultMessage: "Hola! Estoy interesado/a en un arreglo con globos. ¿Me ayudas con información y disponibilidad?",
+  businessName: "Balloonie"
+};
