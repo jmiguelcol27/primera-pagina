83574b4da39524d5aa241b6ddd6574fcf79db400
jmiguelcol27 <jmiguelcol2712@gmail.com>
2026-02-22 21:41:54 -0600

    Revert "feat: replace balloon quantity option with arrangement description textarea"
    This reverts commit 31327b637d0b0d414424737d8b4cb5e0ee08efbf.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SITE_CONFIG = {
   whatsappPhoneE164: "17865992191",
   defaultMessage: "Hola! Estoy interesado/a en un arreglo con globos. ¿Me ayudas con información y disponibilidad?",
-  businessName: "Balloonie"
+  businessName: "Tu Negocio de Globos"
 };
